29eb2922be24d1a8b1404e19c2d7b46eae04b2a8
Suraj <surajutt@iitbhilai.ac.in>
2024-11-15 12:15:51 +0530

    deployment

--- frontend/src/config/api/axios.js
+++ frontend/src/config/api/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL:
-    "http://localhost:3500",
+  baseURL: "https://automatex-leechers.onrender.com",
   headers: { "Content-Type": "application/json" },
 });
